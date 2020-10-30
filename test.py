809dac432fdd1496e366915e5b2f2bb6933a951d
迟慧璇 <chihuixuan@chihuixuandeMacBook-Pro.local>
2020-10-30 18:10:44 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+# import keras
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import cv2
+# import django
+
+# print(keras.__version__)
+# print(pd.__version__)
+# print(cv2.__version__)
+# print(django.__version__)
